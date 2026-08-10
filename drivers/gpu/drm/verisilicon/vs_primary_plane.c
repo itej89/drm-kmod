ef8cba5f8eb2a38b8d5cc859b74f3ff2066715e6
Tej Kiran <kiran.tej@amd.com>
2026-08-09 21:00:17 -0500

    verisilicon: Remove cache flush, JH7110 DMA is coherent

--- drivers/gpu/drm/verisilicon/vs_primary_plane.c
+++ drivers/gpu/drm/verisilicon/vs_primary_plane.c
@@ -129,14 +129,6 @@ static void vs_primary_plane_atomic_update(struct drm_plane *plane,
 
 	dma_addr = vs_fb_get_dma_addr(fb, &state->src);
 
-#ifdef __FreeBSD__
-	{
-		extern void sifive_ccache_flush_range(uint64_t, unsigned long);
-		sifive_ccache_flush_range(dma_addr,
-		    (unsigned long)fb->pitches[0] * state->crtc_h);
-	}
-#endif
-
 	regmap_write(dc->regs, VSDC_FB_ADDRESS(output),
 		     lower_32_bits(dma_addr));
 	regmap_write(dc->regs, VSDC_FB_STRIDE(output),
